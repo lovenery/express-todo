59594ec0121aa072ebda8ab242dbd495a007fed5
Hsu <edittye379@gmail.com>
2017-01-22 10:59:24 +0800

    view && style

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var todoController = require('./controllers/todoController')
 
 var app = express()
 app.set('view engine', 'ejs')
-app.use('/assets', express.static('./public'))
+app.use(express.static('public'))
 app.listen(3000)
 
 todoController(app)
