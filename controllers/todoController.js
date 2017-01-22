594ec8212a836a87a5d5d5413647807c34760bc4
Hsu <edittye379@gmail.com>
2017-01-22 12:13:45 +0800

    db mlab

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,26 +1,53 @@
 const bodyParser = require('body-parser')
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-let data = [ { item: 'mail' }, { item: 'run' }, { item: 'coding'} ]
+// dummy data
+// let data = [ { item: 'mail' }, { item: 'run' }, { item: 'coding'} ]
+
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise // use native mongoose promises﻿
+mongoose.connect('mongodb://test:test@ds039860.mlab.com:39860/todo')
+const todoSchema = new mongoose.Schema({
+  item: String
+})
+let Todo = mongoose.model('Todo', todoSchema)
+// var testdata = Todo({item: 'test'}).save(function (err) {
+//   if (err) { throw err }
+//   console.log('item saved');
+// })
 
 module.exports = function (app) {
 
 app.get('/todo', function (req, res) {
-  res.render('todo', {
-    todos: data
+  Todo.find({}, function (err, data) {
+    if (err) throw err
+    res.render('todo', {
+      todos: data
+    })
   })
+  // res.render('todo', {
+  //   todos: data
+  // })
 })
 
 app.post('/todo', urlencodedParser, function (req, res) {
-  data.push(req.body)
-  res.json(data)
+  var newTodo = Todo(req.body).save(function (err, data) {
+    if (err) throw err
+    res.json(data)
+  })
+  // data.push(req.body)
+  // res.json(data)
 })
 
 app.delete('/todo/:item', function (req, res) {
-  data = data.filter(function (todo) {
-    return todo.item.replace(/ /g, '-') !== req.params.item
+  Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function (err, data) {
+    if (err) throw err
+    res.json(data)
   })
-  res.json(data)
+  // data = data.filter(function (todo) {
+  //   return todo.item.replace(/ /g, '-') !== req.params.item
+  // })
+  // res.json(data)
 })
 
 }
